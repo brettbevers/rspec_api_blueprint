3a61b7c69a6da268191fc7a70b74e8df4b70f76d
Brett M Bevers <brett@nationbuilder.com>
2015-04-14 08:24:48 -0700

    Update rspec_api_blueprint.rb

--- lib/rspec_api_blueprint.rb
+++ lib/rspec_api_blueprint.rb
@@ -1,6 +1,7 @@
 require "rspec_api_blueprint/version"
 require "rspec_api_blueprint/string_extensions"
 
+RESOURCE_REGEXP = /^(.*)\[(.*)\]/
 
 RSpec.configure do |config|
   config.before(:suite) do
@@ -20,31 +21,56 @@ RSpec.configure do |config|
   config.around(:each, type: :request) do |example|
     example_group = example.metadata[:example_group]
     example_groups = []
-
     while example_group
       example_groups << example_group
       example_group = example_group[:example_group]
     end
 
-    @action = example_groups[-2][:description_args].first if example_groups[-2]
-    @group_name = example_groups[-1][:description_args].first
-    file_name = group_name.underscore
+    @level_3 = example_groups[-3][:description_args].first if example_groups[-3]
+    @level_2 = example_groups[-2][:description_args].first if example_groups[-2]  
+    @level_1 = example_groups[-1][:description_args].first 
+    file_name = @level_1.underscore.gsub(/\s+/, '_')
     if defined? Rails
       @file = File.join(Rails.root, "/api_docs/#{file_name}.txt")
     else
       @file = File.join(File.expand_path('.'), "/api_docs/#{file_name}.txt")
-    end
-    
+    end 
+    @action = example.description    
     example.run
   end
 
   config.after(:each, type: :request) do
-    return unless response.nil? || response.status == 401 || response.status == 403 || response.status == 301
+    unless response.nil? || response.status == 401 || response.status == 403 || response.status == 301
 
     File.open(@file, 'a') do |f|
-      f.write "# #{@group_name}"
-      f.write "## #{@action}\n\n" if @action
-        
+      unless $header_written
+        if @level_3
+          f.write "# #{@level_1}\n\n"
+          f.write "## Group #{@level_2}\n\n"
+          f.write "### #{@level_3}\n\n" 
+        elsif @level_2
+          f.write "# Group #{@level_1}\n\n"
+          f.write "## #{@level_2}\n\n"
+        else
+          f.write "# #{@level_1}\n\n"
+        end
+        $header_written = true
+      end
+
+      if @level_3
+        resource_level = '###'
+        action_level = '####'
+      elsif @level_2
+        resource_level = '##'
+        action_level = '###'
+      else
+        resource_level = '#'
+        action_level = '##'
+      end
+  
+      f.write "#{resoure_level} #{$1}[#{$2}?ignore_this=#{SecureRandom.uuid}]"
+      f.write "#{action_level} #{@action}\n\n"
+
       # Request
       request_body = request.body.read
       if request_body.present? 
@@ -52,7 +78,7 @@ RSpec.configure do |config|
 
         # Request Body
         if request_body.present? && 'application/json' == request.content_type.to_s
-          f.write "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(authorization_header ? 12 : 8)
+          f.write "#{JSON.pretty_generate(JSON.parse(request_body))}\n\n".indent(8)
         end
       end
 
